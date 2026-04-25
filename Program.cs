8bd279787313c4f3dbf14d1976d01b5686880dc4
Joana Baptistin <joana.baptistin@gmail.com>
2026-04-24 21:23:10 -0300

    atividade calculo IMC

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double IMC;
+
+Console.Write("Altura (m)...: ");
+string alturaDigitada = Console.ReadLine()!;
+
+double altura = Convert.ToDouble(alturaDigitada);
+
+Console.Write("Peso (kg)...: ");
+string pesoDigitado = Console.ReadLine()!;
+
+double peso = Convert.ToDouble(pesoDigitado);
+
+IMC = peso/Math.Pow(altura, 2);
+
+Console.WriteLine($"\nIMC:{IMC:N2} kg/m²");
